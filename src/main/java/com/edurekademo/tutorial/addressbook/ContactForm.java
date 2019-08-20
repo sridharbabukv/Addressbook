973b18f83879d05ab83861075b2d77a45f29b71c
cicdTestProject <uvrkramakrishna@gmail.com>
2019-08-20 15:19:30 +0530

    change name

--- src/main/java/com/edurekademo/tutorial/addressbook/ContactForm.java
+++ src/main/java/com/edurekademo/tutorial/addressbook/ContactForm.java
@@ -25,7 +25,7 @@ public class ContactForm extends FormLayout {
 
     Button save = new Button("Save", this::save);
     Button cancel = new Button("Cancel", this::cancel);
-    TextField firstName = new TextField("First Name");
+    TextField firstName = new TextField("First name");
     TextField lastName = new TextField("Last Name");
     TextField phone = new TextField("Mobile");
     TextField email = new TextField("Email");
