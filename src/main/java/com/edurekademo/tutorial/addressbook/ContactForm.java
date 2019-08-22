4c3e4ef222321e7c1edea2355f359fde9094d964
sridharbabukv <52557862+sridharbabukv@users.noreply.github.com>
2019-08-22 12:05:22 +0530

    THis is commitedd

--- src/main/java/com/edurekademo/tutorial/addressbook/ContactForm.java
+++ src/main/java/com/edurekademo/tutorial/addressbook/ContactForm.java
@@ -25,11 +25,11 @@ public class ContactForm extends FormLayout {
 
     Button save = new Button("Save", this::save);
     Button cancel = new Button("Cancel", this::cancel);
-    TextField firstName = new TextField("First Name");
-    TextField lastName = new TextField("Last Name");
+    TextField firstName = new TextField("FirstName");
+    TextField lastName = new TextField("LastName");
     TextField phone = new TextField("Mobile");
     TextField email = new TextField("Email");
-    DateField birthDate = new DateField("Birth Date");
+    DateField birthDate = new DateField("BirthDate");
 
     Contact contact;
 
