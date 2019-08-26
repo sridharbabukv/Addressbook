94aa126e78e4778773733ac19c15c03ea1b8e900
sridharbabukv <52557862+sridharbabukv@users.noreply.github.com>
2019-08-26 14:49:46 +0530

    Mobile Number label changed

--- src/main/java/com/edurekademo/tutorial/addressbook/ContactForm.java
+++ src/main/java/com/edurekademo/tutorial/addressbook/ContactForm.java
@@ -25,9 +25,9 @@ public class ContactForm extends FormLayout {
 
     Button save = new Button("Save", this::save);
     Button cancel = new Button("Cancel", this::cancel);
-    TextField firstName = new TextField("First Name");
+    TextField firstName = new TextField("FirstName");
     TextField lastName = new TextField("Last Name");
-    TextField phone = new TextField("Mobile");
+    TextField phone = new TextField("Mobile Number");
     TextField email = new TextField("Email");
     DateField birthDate = new DateField("BirthDate");
 
