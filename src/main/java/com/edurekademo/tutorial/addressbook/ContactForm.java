353493cc58fd950f02fcccffa92903c24bc95c08
Vijay Kumar <kellavijay@gmail.com>
2019-08-27 17:54:18 +0530

    Mobile Number Label change
    I have changed the Mobile Number to Mobile No

--- src/main/java/com/edurekademo/tutorial/addressbook/ContactForm.java
+++ src/main/java/com/edurekademo/tutorial/addressbook/ContactForm.java
@@ -27,7 +27,7 @@ public class ContactForm extends FormLayout {
     Button cancel = new Button("Cancel", this::cancel);
     TextField firstName = new TextField("First Name");
     TextField lastName = new TextField("Last Name");
-    TextField phone = new TextField("Mobile Number");
+    TextField phone = new TextField("Mobile No");
     TextField email = new TextField("Email");
     DateField birthDate = new DateField("BirthDate");
 
