dd27c11d20e09c3f4056343a233b32061140f2c9
kellavijay@gmail.com <kellavijay@gmail.com>
2019-08-29 10:18:12 +0530

    Changed Address boook UI Page Label

--- src/main/java/com/edurekademo/tutorial/addressbook/AddressbookUI.java
+++ src/main/java/com/edurekademo/tutorial/addressbook/AddressbookUI.java
@@ -71,7 +71,7 @@ public class AddressbookUI extends UI {
          */
         newContact.addClickListener(e -> contactForm.edit(new Contact()));
 
-        filter.setInputPrompt("Filter contacts...");
+        filter.setInputPrompt("Filter Contacts ....");
         filter.addTextChangeListener(e -> refreshContacts(e.getText()));
 
         contactList
