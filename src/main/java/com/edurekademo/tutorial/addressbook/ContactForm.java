4df8ee1e8396ce4799a4eb47e7aa527879c005c2
sridharbabukv <52557862+sridharbabukv@users.noreply.github.com>
2019-09-09 14:06:07 +0530

    Update ContactForm.java

--- src/main/java/com/edurekademo/tutorial/addressbook/ContactForm.java
+++ src/main/java/com/edurekademo/tutorial/addressbook/ContactForm.java
@@ -34,7 +34,7 @@ public class ContactForm extends FormLayout {
     TextField phone = new TextField("Contact Number");
     TextField email = new TextField("Email Address");
 
-    DateField birthDate = new DateField("BirthDate");
+    DateField birthDate = new DateField("Birth Date");
 
     Contact contact;
 
